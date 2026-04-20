a8bd14a8389970f819b2a74ac9ae494451a2b9ba
vequr <u56175@student.wat.edu.pl>
2026-04-20 15:31:28 +0200

    Update ex_10_01.py

--- ex_10/ex_10_01.py
+++ ex_10/ex_10_01.py
@@ -1,9 +1,23 @@
 file = open('mbox-short.txt')
+
 wrd_dict = {}
+
 for line in file:
     words = line.split()
     if len(words) < 2:
         continue
     if words[0] != 'From':
         continue
-    
+
+    email = words[1]
+
+    wrd_dict[email] = wrd_dict.get(email, 0) + 1
+
+print(wrd_dict)
+
+lista = []
+for email,count in wrd_dict.items():
+    lista.append((count, email))
+
+lista.sort(reverse=True)
+print(lista[0])
